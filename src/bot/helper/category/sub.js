28a68843d550d4213a22d24764a5dd93c23fcb5f
Akmaljon571 <akmaljon079@gmail.com>
2023-09-17 16:59:54 +0300

    finishcha

--- src/bot/helper/category/sub.js
+++ src/bot/helper/category/sub.js
@@ -11,8 +11,7 @@ const sub = async (msg) => {
     const text = msg.text.split('. ').slice(1).join(' ')
 
     const findSub = await SubCategory.findOne({ title: text }).populate('product')
-    // const img = `https://api.telegram.org/bot${process.env.TOKEN}/getFile?file_id=${findSub.img}`
-     
+    const img = `https://api.telegram.org/bot${process.env.TOKEN}/getFile?file_id=${findSub.img}`
     if (findSub.product?.length) {
         let a = ''
         findSub.product.map((e, i) => a += `\n${i + 1}. Narxi: ${summa(e?.price)} so'm`)
